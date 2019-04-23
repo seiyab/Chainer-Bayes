289f13ac2021ef0c559a8cef51e9a40b3683a3c9
seiya <r108338@yahoo.co.jp>
2019-04-23 23:13:14 +0900

    正規分布以外を使えるようにした

--- stochastic_variable.py
+++ stochastic_variable.py
@@ -2,28 +2,55 @@ import copy
 import chainer as ch
 from chainer import distributions as dist
 
-class StochasticVariable:
+class StochasticVariable(object):
+    def __init__(self, distribution_class, *params):
+        self._distribution_class = distribution_class
+        self._params = params
+
+        self._is_sampled = False
+        self._is_conditioned = False
+        self._value = None
+        self._dependencies = [param for param in self._params if isinstance(param, StochasticVariable)]
+
     def sample(self):
-        raise NotImplementedError()
+        if not self.is_determined:
+            realized_params = [
+                param.sample() if isinstance(param, StochasticVariable) else param
+                for param in self._params
+            ]
+            distribution = self._distribution_class(*realized_params)
+            self._value = distribution.sample()
+            self._is_sampled = True
+        return self._value
 
     def condition(self, data):
-        raise NotImplementedError()
+        assert not self.is_sampled
+        assert not self.is_conditioned
+        self._value = copy.copy(data)
+        self._is_conditioned = True
+        return self._value
 
     @property
     def log_prob(self):
-        raise NotImplementedError()
-
-    @property
-    def dependencies(self):
-        raise NotImplementedError()
+        assert self.is_determined
+        assert all(dependency.is_determined for dependency in self._dependencies)
+        realized_params = [
+            param._value if isinstance(param, StochasticVariable) else param
+            for param in self._params
+        ]
+        return self._distribution_class(*realized_params).log_prob(self._value)
 
     @property
     def is_sampled(self):
-        raise NotImplementedError()
+        return self._is_sampled
 
     @property
     def is_conditioned(self):
-        raise NotImplementedError()
+        return self._is_conditioned
+
+    @property
+    def dependencies(self):
+        return self._dependencies
 
     @property
     def is_determined(self):
@@ -58,7 +85,6 @@ class NormalStochasticVariable(StochasticVariable):
     def log_prob(self):
         assert self.is_determined
         return dist.Normal(self._mu, self._sigma).log_prob(self._value)
-
 
     @property
     def is_sampled(self):
