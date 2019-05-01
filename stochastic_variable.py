8678a1b8d8a8b94d91ac3fb10779e969310abbf1
seiya <r108338@yahoo.co.jp>
2019-05-01 20:14:22 +0900

    bernoulli -> categorical because of a bug in chainer

--- stochastic_variable.py
+++ stochastic_variable.py
@@ -6,20 +6,26 @@ class StochasticVariable(object):
     def __init__(self, distribution_class, *params, **kwparams):
         self._distribution_class = distribution_class
         self._params = params
-        self._sample_shape = kwparams['sample_shape'] if 'sample_shape' in kwparams else ()
+        self._sample_shape = kwparams.pop('sample_shape', ())
+        self._kwparams = kwparams
 
         self._is_sampled = False
         self._is_conditioned = False
         self._value = None
-        self._dependencies = [param for param in self._params if isinstance(param, StochasticVariable)]
+        self._dependencies = [param for param in self._params if isinstance(param, StochasticVariable)] \
+                + [param for param in self._kwparams.values() if isinstance(param, StochasticVariable)]
 
     def sample(self):
         if not self.is_determined:
             realized_params = [
-                param.sample() if isinstance(param, StochasticVariable) else param
-                for param in self._params
-            ]
-            distribution = self._distribution_class(*realized_params)
+                    param.sample() if isinstance(param, StochasticVariable) else param
+                    for param in self._params
+                    ]
+            realized_kwparams = {
+                    key: param.sample if isinstance(param, StochasticVariable) else param
+                    for key, param in self._kwparams.items()
+                    }
+            distribution = self._distribution_class(*realized_params, **realized_kwparams)
             self._value = distribution.sample(sample_shape=self._sample_shape)
             self._is_sampled = True
         return self._value
@@ -39,7 +45,15 @@ class StochasticVariable(object):
             param._value if isinstance(param, StochasticVariable) else param
             for param in self._params
         ]
-        return self._distribution_class(*realized_params).log_prob(self._value)
+        realized_kwparams = {
+                key: param._value if isinstance(param, StochasticVariable) else param
+                for key, param in self._kwparams.items()
+        }
+        return self._distribution_class(*realized_params, **realized_kwparams).log_prob(self._value)
+
+    @property
+    def log_prob_sum(self):
+        return self._value.xp.sum(self.log_prob)
 
     @property
     def is_sampled(self):
--- test_inference.py
+++ test_inference.py
@@ -5,11 +5,11 @@ from stochastic_variable import StochasticVariable
 from inference import MaximumLilekihoodEstimation
 
 def run():
-    test_maximum_likelihood_small_steps()
+    test_maximum_likelihood_one_step()
     test_maximum_likelihood_fixed_point()
-    test_maxlimum_likelihood_bernoulli()
+    test_maxlimum_likelihood_categorical()
 
-def test_maximum_likelihood_small_steps():
+def test_maximum_likelihood_one_step():
     np.random.seed(0)
 
     param_mu_init = 3.
@@ -17,19 +17,21 @@ def test_maximum_likelihood_small_steps():
     param_mu = ch.Parameter(np.array(param_mu_init))
     param_sigma = ch.Parameter(np.array(param_sigma_init))
 
-    model = lambda: {"x": StochasticVariable(dist.Normal, param_mu, param_sigma)}
-    infer = MaximumLilekihoodEstimation(model, mu=param_mu, sigma=param_sigma)
+    model = lambda: {"x": StochasticVariable(dist.Normal, param_mu, param_sigma, sample_shape=(30,))}
+    infer = MaximumLilekihoodEstimation(model, mu=param_mu)
     optimizer = ch.optimizers.SGD(lr=0.05).setup(infer)
 
     data = ch.Variable(np.random.randn(30))
-    for d in data:
-        infer.cleargrads()
-        loss = infer({"x": d})
-        loss.backward()
-        optimizer.update()
+    infer.cleargrads()
+    loss_before = infer({"x": data})
+    loss_before.backward()
+    optimizer.update()
 
-    eps = 0.2
-    assert 0-eps < param_mu.data < param_mu_init
+    loss_after = infer({"x": data})
+
+    assert param_mu.data < param_mu_init
+    assert param_sigma.data == param_sigma_init
+    assert loss_before.data > loss_after.data
 
 def test_maximum_likelihood_fixed_point():
     np.random.seed(0)
@@ -39,14 +41,14 @@ def test_maximum_likelihood_fixed_point():
     param_mu = ch.Parameter(np.array(param_mu_init))
     param_sigma = ch.Parameter(np.array(param_sigma_init))
 
-    model = lambda: {"x": StochasticVariable(dist.Normal, param_mu, param_sigma)}
+    model = lambda: {"x": StochasticVariable(dist.Normal, param_mu, param_sigma, sample_shape=(30,))}
     infer = MaximumLilekihoodEstimation(model, mu=param_mu, sigma=param_sigma)
     optimizer = ch.optimizers.SGD(lr=0.05).setup(infer)
 
-    data = ch.Variable(np.random.randn(50))
-    for d in data:
+    for _ in range(10):
+        data = ch.Variable(np.random.randn(30))
         infer.cleargrads()
-        loss = infer({"x": d})
+        loss = infer({"x": data})
         loss.backward()
         optimizer.update()
 
@@ -54,24 +56,21 @@ def test_maximum_likelihood_fixed_point():
     assert 0-eps < param_mu.data < 0+eps
     assert 1-eps < param_sigma.data < 1+eps
 
-def test_maxlimum_likelihood_bernoulli():
+def test_maxlimum_likelihood_categorical():
     np.random.seed(0)
 
-    param_p_init = 0.3
-    param_p = ch.Parameter(np.array(param_p_init))
+    param_logit_init = np.zeros(3)
+    param_logit = ch.Parameter(np.array(param_logit_init))
 
-    model = lambda: {"b": StochasticVariable(dist.Bernoulli, param_p)}
-    infer = MaximumLilekihoodEstimation(model, p=param_p)
+    model = lambda: {"x": StochasticVariable(dist.Categorical, logit=param_logit, sample_shape=(10,))}
+    infer = MaximumLilekihoodEstimation(model, logit=param_logit)
     optimizer = ch.optimizers.SGD(lr=0.05).setup(infer)
 
-    data = ch.Variable(np.array([0, 1, 0, 1, 0, 1, 0, 1, 0, 1], dtype=np.float))
-    for d in data:
-        infer.cleargrads()
-        loss = infer({"b": d})
-        loss.backward()
-        optimizer.update()
+    data = ch.Variable(np.array([0.]*5 + [1.]*5))
+    infer.cleargrads()
+    loss = infer({"x": data})
+    loss.backward()
+    optimizer.update()
 
-    eps = 0.1
-
-    assert param_p_init < param_p.data < 0.5+eps
+    assert param_logit_init[0] < param_logit[0].data
 
